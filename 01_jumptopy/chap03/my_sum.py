58afea173b143ad5ce1e18c754c84b66c53cc36d
hklee <hklee0724@gmail.com>
2018-03-20 10:30:25 +0900

    .

---
+++ 01_jumptopy/chap03/my_sum.py
@@ -0,0 +1,20 @@
+def my_sum(a,b):
+    result = a+b
+    print("Calculation Start!")
+    print("First Parameter a: ",a)
+    print("Second Parameter b: ",b)
+    print("Internal Expression: a+b")
+    print("Calculation Result: ",result)
+    print("Calculation End")
+    print("")
+
+    return result
+
+print("my_sum program is initiated..")
+print("")
+my_sum(1,2)
+my_sum(3,2)
+my_sum(1,4)
+my_sum(5,2)
+my_sum(11,2)
+print("my_sum program is terminated..")
---
+++ 01_jumptopy/chap03/my_sum2.py
@@ -0,0 +1,20 @@
+def my_sum(a,b):
+    result = a+b
+    print("연산 시작")
+    print("첫 번째 인자 a: ",a)
+    print("두 번째 인자 b: ",b)
+    print("내부 연산식: a+b")
+    print("연산 결과: ",result)
+    print("연산 종료")
+    print("")
+
+    return result
+
+print("my_sum 프로그램을 시작합니다.")
+print("")
+print(my_sum(1,2))
+print(my_sum(3,2))
+print(my_sum(1,4))
+print(my_sum(5,2))
+print(my_sum(11,2))
+print("my_sum 프로그램을 종료합니다.")
